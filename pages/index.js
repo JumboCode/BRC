7a8273f3ec1e09117ddd8a2610a9da47a8141737
claireboals <claireboals@gmail.com>
2018-09-26 21:21:55 -0400

    Added unstyled search bar to page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { SearchBar } from "../components";
+
 const titleStyle = {
   textAlign: 'center',
   fontSize: '40px',
@@ -6,7 +8,10 @@ const titleStyle = {
 };
 
 const Index = () => (
+  <>
     <div style={titleStyle}>Love is love</div>
+    <SearchBar />
+  </>
 );
 
 export default Index;
