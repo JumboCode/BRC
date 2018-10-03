a4d91ef3020e1df7174f54294f88b78937981444
Steph Xu <stephxuzy96@gmail.com>
2018-10-03 19:59:46 -0400

    Centered search bar and removed onChange readonly warning

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,10 +1,18 @@
 import React from "react";
 
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+}
+
 const SearchBar = () => (
-    <div>
+    <div style={styles.container}>
       <form>
-        <input type="text" value="type here"/>
-        <input type="submit" value="search"/>
+        <input type="text" value="type here" onChange={() => console.log("input change")}/>
+        <input type="submit" value="search" onChange={() => console.log("submit address")}/>
       </form>
     </div>
 );
--- pages/steph.js
+++
@@ -1,5 +0,0 @@
-const Steph = () => (
-    <h1>Ahh</h1>
-);
-
-export default Steph;
