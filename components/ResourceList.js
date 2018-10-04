fe1ac1ffdd2524c6c566edc5d33bace93482476f
jlemus1234 <jlemus1234@gmail.com>
2018-10-04 10:16:05 -0400

    Skeleton for resourcelist

---
+++ components/ResourceList.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+// An individual accordian thing
+class ResourceState extends React.Component {
+    // Contain the state row, and its resources are children
+
+}
+
+// A resource within some accordian
+class ResourceRow extends React.Component {
+
+}
+
+// pass the styles in using some prop type thing
+// Contains multiple resource components
+class ResourceList extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+        };
+
+    }
+
+    // have a list of states and a list of resources
+    
+
+
+    render() {
+        <div>Welcome to React</div>
+
+    }
+
+}
+
+
+export default TitleBar;
