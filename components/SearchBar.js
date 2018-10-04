e78180b0ddd30fcdee5cf213ae4f90fa9e35e11f
Steph Xu <stephxuzy96@gmail.com>
2018-10-03 21:22:43 -0400

    Added search bar value change

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -8,13 +8,27 @@ const styles = {
   },
 }
 
-const SearchBar = () => (
-    <div style={styles.container}>
-      <form>
-        <input type="text" value="type here" onChange={() => console.log("input change")}/>
-        <input type="submit" value="search" onChange={() => console.log("submit address")}/>
-      </form>
-    </div>
-);
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "type here" };
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+    console.log(this.state);
+  };
+
+  render () {
+    return (
+        <div style={styles.container}>
+          <form>
+            <input type="text" value={this.state.value} onChange={(e) => this.handleChange(e)}/>
+            <input type="submit" value="search" onChange={() => {return;} } />
+          </form>
+        </div>
+    );
+  }
+}
 
 export default SearchBar;
