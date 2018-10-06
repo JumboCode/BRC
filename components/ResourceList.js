19ef9494375f4bdb955f1598aca5ddf74b06a08d
jlemus1234 <jlemus1234@gmail.com>
2018-10-06 13:31:47 -0400

    working on adding functions to work wtih children of accordian

--- components/ResourceList.js
+++ components/ResourceList.js
@@ -1,33 +1,43 @@
 import React from "react";
 
 // An individual accordian thing
-class ResourceState extends React.Component {
+class AccordianTitle extends React.Component {
     // Contain the state row, and its resources are children
 
 }
 
 // A resource within some accordian
-class ResourceRow extends React.Component {
+class AccordianContents extends React.Component {
 
 }
 
 // pass the styles in using some prop type thing
 // Contains multiple resource components
-class ResourceList extends React.Component{
+// Takes a list of objects, with a state string and
+// a list of resources
+// [{"State": [Resource1, Resource2, Resource3]}]
+class Accordian extends React.Component{
     constructor() {
-        super();
+        super(props);
+        // set initial state
         this.state = {
+            open:false
         };
-
+        // no methods to bind        
     }
 
+    renderChildren(){
+        //return React.Children.map(this.props.children, 
+    }
+
+
     // have a list of states and a list of resources
-    
-
-
     render() {
-        <div>Welcome to React</div>
-
+        return (
+        <div classNAme = "accordian">
+        {}
+        </div>
+        )
     }
 
 }
