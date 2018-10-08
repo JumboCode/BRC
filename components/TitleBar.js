457baeca6bd089c3820f95f12683cd6b07cb7552
Sabrina Wen <sabrinawn253@gmail.com>
2018-10-08 17:40:29 -0400

    styled titlebar with flexbox

--- components/TitleBar.js
+++ components/TitleBar.js
@@ -1,7 +1,31 @@
-import React from "react";
+import { Column, Row } from 'simple-flexbox';
+
+var titleStyle = {
+  fontWeight: 'bold',
+  fontSize: 50,
+  fontFamily: 'BlinkMacSystemFont'
+};
 
 const TitleBar = () => (
-    <div>Welcome to React</div>
+  <Row horizontal = 'center' style = {{margin: 10}}>
+    <Column flexGrow = {1}>
+      <Row>
+        <div style = {titleStyle}>Bi Spot: Find a group near you.</div>
+      </Row>
+      <Row>
+        <span> table text </span>
+      </Row>
+    </Column>
+    <Column flexGrow={1} horizontal='center'>
+      insert map here
+    </Column>
+  </Row>
 );
 
 export default TitleBar;
+
+/* Questions:
+ Why export instead of using ReactDOM.render(element, etc)?
+ Also why does export display the contents of index.js which is set
+ up in the same way, but not for TitleBar.js?
+ */
