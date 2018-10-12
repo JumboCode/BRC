4a88d75ef7f2264bdb4a565ec8f7aa4f08c3d209
Steph Xu <stephxuzy96@gmail.com>
2018-10-11 20:31:47 -0400

    Cleaned up junk code

--- components/TitleBar.js
+++ components/TitleBar.js
@@ -1,22 +1,7 @@
 import React from "react";
 
-const titleStyle = {
-
-};
-
 const TitleBar = () => (
-<<<<<<< HEAD
     <div></div>
-=======
-    <div style={titleStyle}>Welcome to React</div>
->>>>>>> pop-up
 );
 
 export default TitleBar;
-
-/*
-class TitleBar extends React.Component {
-	render () {
-		return open ? <div>Welcome to React</div> : <div> No welcome </div>;
-	}
-}*/
