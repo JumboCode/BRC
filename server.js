24c55719f03bb5b1a50ed073c0383982bb553452
Steph Xu <stephxuzy96@gmail.com>
2018-10-13 14:19:07 -0400

    Added mongo url

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var mongodb = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
 var mongoURI = process.env.MONGODB_URI || 
                process.env.MONGOLAB_URI || 
-               process.env.MONGOHQ_URL;
+               process.env.MONGOHQ_URL || 
+               'mongodb://brcjumbocode:BRC2018@ds123971.mlab.com:23971/brc2018';
+
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const app = next(process.env.NODE_ENV !== "production");
@@ -21,9 +23,13 @@ app.prepare()
             return {};
         });
 
-        //fetch locations data from database as list
-        server.get('/locations.json', function(req, res) {
-            MongoClient.connect(mongoURI, { useNewUrlParser: true }, function(err, client) {
+        //list locations at /locations
+        server.get('/locations', function(req, res) {
+            res.set('Content-Type', 'text/html');
+            var indexPage = '';
+
+            //make mongodb connection
+            MongoClient.connect(mongoURI, { useNewUrlParser: true }, function (err, client) {
                 if (err) throw err;
                 var db = client.db('brc2018');    //database name
 
@@ -33,14 +39,23 @@ app.prepare()
                     //equivalent to 'db.locations.find()' in MongoDB client shell
                     collection.find().toArray(function(err, results) {
 
-                        if (!err)   //don't send anything if error for now
-                        {
-                            res.send(results);
+                        //all results of db.locations.find() will go into...
+                        //'results' will be an array (or list)
+                        if (!err) {
+                            indexPage += "<!DOCTYPE HTML><html><head><title>Locations</title></head><body><h1>Locations:</h1>";
+                            for (var count = 0; count < results.length; count++) {
+                                indexPage += "<p>Location " + count + ": " + results[count].loc + "</p>";
+                            }
+                            indexPage += "</body></html>"
+                            res.send(indexPage);
+                        } else {
+                            res.send('<!DOCTYPE HTML><html><head><title>Locations</title></head><body><h1>Whoops, something went terribly wrong!</h1></body></html>');
                         }
                     });
                 });
             });
         });
+
 
         server.get("*", (req, res) => {
             return handle(req, res);
