f30d0fb176605b2b803b07c3fd0d821a10741af4
Naoki Okada <naokiokada11@gmail.com>
2018-10-21 17:00:29 -0400

    Still working

--- components/SocialMedia.js
+++ components/SocialMedia.js
@@ -1,10 +1,17 @@
 import React from "react";
 
-const style = {
-
+const styles = {
+    navBarContainer: {
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: "grey",
+        padding: "20px",
+    },
 }
 
-const NavBar = () => (
+const SocialMedia = () => (
     <>
         <div>
 
