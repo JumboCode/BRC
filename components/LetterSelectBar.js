e94821c886e09ada9490b2d77738d0cc29a2a05d
jlemus1234 <jlemus1234@gmail.com>
2018-10-21 17:49:23 -0400

    Generating an unlcikable bar separated alphabet and displaying on test page

---
+++ components/LetterSelectBar.js
@@ -0,0 +1,85 @@
+import { Component } from "react"
+
+
+class Letter extends Component{
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    onClick(){
+        // Call some function passed in by the parent
+        this.props.letterClicked(this.props.letter)
+    }
+
+    render(){
+        return(
+            <p>{this.props.letter}</p>
+        )
+    }
+
+}
+
+Letter.propTypes = {
+    letter: React.PropTypes.string.isRequired,
+    letterClicked: React.PropTypes.function.isRequired
+};
+
+Letter.defaultProps = {
+    letterClicked: () => {}
+
+}
+
+
+const LetterSelectBarStyle = {
+    display: 'flex'
+}
+
+
+class LetterSelectBar extends Component{
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    // Generate a list of the alphabet starting from 
+    // startChar and ending at endChar inclusive
+    genAlphabet(startChar, endChar){
+        var i = startChar.charCodeAt(0)
+        var j = endChar.charCodeAt(0)
+        var alphabet = []
+
+        for(i; i <= j; i++){
+            alphabet.push(String.fromCharCode(i))
+        }
+
+        return alphabet
+    }
+
+
+
+
+    render(){
+        var alphabet = (this.genAlphabet("A", "Z"))
+        console.log(alphabet)
+
+        var sections = []
+        for (var i = 0; i < alphabet.length; i++){
+            var character = alphabet[i]
+            sections.push(<Letter letter = {character}></Letter>)
+            if (i != alphabet.length - 1){
+                sections.push(<p> | </p>)
+            }
+        }
+
+        
+
+        return(
+            <div style = {LetterSelectBarStyle} className = "LetterSelectBar">
+                      {sections}
+            </div>
+        )
+    }
+}
+
+export default LetterSelectBar;
---
+++ pages/test.js
@@ -0,0 +1,20 @@
+import LetterSelectBar from "../components/LetterSelectBar";
+import { Component } from "react";
+
+class Test extends Component {
+    constructor (props){
+        super(props);
+        this.state = {};
+    }
+
+    render () {
+        return (
+            <LetterSelectBar/>
+
+        )
+    }
+
+}
+
+
+export default Test;
