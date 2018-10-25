920ebe75ee56c3f632eb0ce169c0e4b53d65e352
Steph Xu <stephxuzy96@gmail.com>
2018-10-24 20:33:56 -0400

    Removed Google Map API error by replacing react-google-maps with google-map-react

--- components/MapContainer.js
+++ components/MapContainer.js
@@ -1,32 +1,41 @@
 import React from "react";
 import { compose, withProps } from "recompose";
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import GoogleMapReact from "google-map-react";
 import getConfig from "next/config";
 const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
 
-const MyMapComponent = compose(
-    withProps({
-      googleMapURL: ("https://maps.googleapis.com/maps/api/js?key=" + publicRuntimeConfig.MAP_KEY),
-      loadingElement: <div style={{ height: `100%` }} />,
-      containerElement: <div style={{ height: `400px` }} />,
-      mapElement: <div style={{ height: `100%` }} />,
-    }),
-    withScriptjs,
-    withGoogleMap
-  )((props) =>
-    <GoogleMap
-      defaultZoom={8}
-      defaultCenter={{ lat: 42.4075, lng: -71.1190}}
-    >
-      {props.isMarkerShown && <Marker position={{  lat: 42.4075, lng: -71.1190 }} onClick={props.onMarkerClick} />}
-    </GoogleMap>
-  )
+// const MyMapComponent = compose(
+//     withProps({
+//       googleMapURL: ("https://maps.googleapis.com/maps/api/js?key=" + publicRuntimeConfig.MAP_KEY),
+//       loadingElement: <div style={{ height: `100%` }} />,
+//       containerElement: <div style={{ height: `400px` }} />,
+//       mapElement: <div style={{ height: `100%` }} />,
+//     }),
+//     withScriptjs,
+//     withGoogleMap
+//   )((props) =>
+//     <GoogleMap
+//       defaultZoom={8}
+//       defaultCenter={{ lat: 42.4075, lng: -71.1190}}
+//     >
+//       {props.isMarkerShown && <Marker position={{  lat: 42.4075, lng: -71.1190 }} onClick={props.onMarkerClick} />}
+//     </GoogleMap>
+//   )
   
   class MapContainer extends React.PureComponent {
     state = {
       isMarkerShown: false,
     }
-  
+
+    static defaultProps = {
+      center: {
+        lat: 42.4075,
+        lng: -71.1190
+      },
+      zoom: 8
+    };
+
     componentDidMount() {
       this.delayedShowMarker();
     }
@@ -44,12 +53,13 @@ const MyMapComponent = compose(
   
     render() {
       return (
-        <>
-          <MyMapComponent
-            isMarkerShown={this.state.isMarkerShown}
-            onMarkerClick={this.handleMarkerClick}
+        <div style={{ height: `400px` }}>
+          <GoogleMapReact 
+            bootstrapURLKeys={{ key: publicRuntimeConfig.MAP_KEY }}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}          
           />
-        </>
+        </div>
       );
     }
   }
