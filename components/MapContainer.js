c330cd4c7c1bffad37bdd1d979f4e30ef79d92b8
jingqi <jingqi.yang@tufts.edu>
2018-10-28 22:46:32 -0400

    started rendering markers at all locations + converted from 2 space indent to 4 space indent

--- components/MapContainer.js
+++ components/MapContainer.js
@@ -5,82 +5,103 @@ import GoogleMap from "google-map-react";
 
 class MapContainer extends React.Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      userLocation: {
-        lat: 42.348591,
-        lng: -71.073051
-      },
-      zoom: 13,
-      markers: [],
-      map: null,
-      maps: null
-    }
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			userLocation: {
+				lat: 42.348591,
+				lng: -71.073051
+			},
+			zoom: 13,
+			markers: [],
+			map: null,
+			maps: null
+		}
+	}
 
-  renderMarkers(map, maps) {
-    //make google map vars available to rest of component
-    this.state.map = map;
-    this.state.maps = maps;
+	renderMarkers(map, maps) {
+		//make google map vars available to rest of component
+		this.state.map = map;
+		this.state.maps = maps;
 
-    //render marker at bisexual resource center (with hard-coded coords)
-    //with current implementation, it will always be on map
-    this.state.markers.push(
-      new maps.Marker({
-        position: {lat: 42.348591, lng: -71.073051},
-        map,
-        title: "Bisexual Resource Center"
-      })
-    );
+		//render marker at bisexual resource center (with hard-coded coords)
+		//with current implementation, it will always be on map
+		this.state.markers.push(
+			new maps.Marker({
+				position: {lat: 42.348591, lng: -71.073051},
+				map,
+				title: "Bisexual Resource Center"
+			})
+		);
 
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({
-          userLocation: {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          }
-        });
+        let locationData = this.props.locations[0]["states"];
 
-        //must store in local temp variables to use
-        let map = this.state.map; 
-        let maps = this.state.maps;
+        console.log(locationData);
 
-        //render marker at user's location
-        this.state.markers.push(
-          new maps.Marker({
-            position: {lat: this.state.userLocation.lat, lng: this.state.userLocation.lng},
-            map,
-            title: "You are here!"
-          })
-        );
-      },
-      (error) => console.log(error)
-    );
-  }
+        for(let state in locationData){
+            if (locationData.hasOwnProperty(state)) {
+                for (let resource in locationData[state])
+                {
+                    console.log(resource);
+                    // this.state.markers.push(
+                    //     new maps.Marker({
+                    //         position: {lat: , lng: },
+                    //         map,
+                    //         title: resource
+                    //     })
+                    // );
+                }
+            }
+        }
 
-  render() {
+        //get user's location in case "searched" prop is undefined
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+				this.setState({
+					userLocation: {
+						lat: position.coords.latitude,
+						lng: position.coords.longitude
+					}
+				});
 
-    //SUCCESSFULLY LOADED GOOGLE MAPS MARKER!! :D
-    //uses onGoogleApiLoaded to access google maps api directly
-    //https://stackoverflow.com/questions/41405343/adding-marker-to-google-maps-in-google-map-react
-    //https://github.com/google-map-react/google-map-react/blob/master/API.md
+				//must store in local temp variables to use
+				let map = this.state.map; 
+				let maps = this.state.maps;
 
-    return (
-      <div style={{ height: `400px` }}>
-        <GoogleMap 
-          bootstrapURLKeys={{ key: publicRuntimeConfig.MAP_KEY }}
-          defaultCenter={{lat: 42.348591, lng: -71.073051}}
-          center={this.state.userLocation}
-          defaultZoom={this.state.zoom}
-          onGoogleApiLoaded={({map, maps}) => this.renderMarkers(map, maps)}
-          yesIWantToUseGoogleMapApiInternals
-        >
-        </GoogleMap>
-      </div>
-    );
-  }
+				//render marker
+				this.state.markers.push(
+					new maps.Marker({
+						position: {lat: this.state.userLocation.lat, lng: this.state.userLocation.lng},
+						map,
+						title: "You are here!"
+					})
+				);
+			},
+			(error) => console.log(error)
+		);
+	}
+
+	render() {
+
+		//SUCCESSFULLY LOADED GOOGLE MAPS MARKER!! :D
+		//uses onGoogleApiLoaded to access google maps api directly
+		//https://stackoverflow.com/questions/41405343/adding-marker-to-google-maps-in-google-map-react
+		//https://github.com/google-map-react/google-map-react/blob/master/API.md
+
+		return (
+			<div style={{ height: `400px` }}>
+				<GoogleMap 
+					bootstrapURLKeys={{ key: publicRuntimeConfig.MAP_KEY }}
+					defaultCenter={{lat: 42.348591, lng: -71.073051}}
+					center={this.state.userLocation}
+					defaultZoom={this.state.zoom}
+					onGoogleApiLoaded={({map, maps}) => this.renderMarkers(map, maps)}
+					yesIWantToUseGoogleMapApiInternals
+				>
+				</GoogleMap>
+			</div>
+		);
+	}
 }
 
 export default MapContainer;
