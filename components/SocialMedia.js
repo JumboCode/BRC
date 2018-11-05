8af2c2c6dfd3e75a04e5be929d44d9f878563dd3
Naoki Okada <naokiokada11@gmail.com>
2018-11-04 21:59:54 -0500

    Added social media icons

--- components/SocialMedia.js
+++ components/SocialMedia.js
@@ -10,25 +10,6 @@ const indiv = {
     paddingRight: "20px"
 }
 
-class Email extends React.Component {
-    state = { showing: true };
-
-    render() {
-        const { showing } = this.state;
-        return (
-            <div>
-                <img style={indiv} onClick={() => this.setState({ showing: !showing })} src="/static/images/email.png" alt="Email" width="20" height="20"/>
-                { showing 
-                    // ? <div>brc@biresource.org</div>
-                    // : null
-                    ? null
-                    : <p>brc@biresource.org</p>
-                }
-            </div>  
-        )
-    }
-}
-
 const SocialMedia = () => (
     <>
         <div style={icons}>
@@ -46,8 +27,6 @@ const SocialMedia = () => (
                 <img src="/static/images/twitter.png" alt="Twitter Logo"
                  width="20" height="20"/>
             </a>
-
-            <Email />
 
             <a style={indiv} href="https://biresource.org/" target="_blank">
                 <img src="/static/images/brc.png" alt="BRC Logo"
