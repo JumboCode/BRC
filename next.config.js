b19bcbfbdebc9d26db3a2b2ebe57e9491fa3a3e8
Steph Xu <stephxuzy96@gmail.com>
2018-11-17 16:53:52 -0500

    I threw in a css loader wow

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@
 
 const { parsed: localEnv } = require('dotenv').config()
 const webpack = require('webpack')
+var path = require('path');
 
 module.exports = {
     serverRuntimeConfig: { // Will only be available on the server side
@@ -15,8 +16,30 @@ module.exports = {
     module: {
       rules: [
         {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
+        {
           test: /\.css$/,
-          use: [ 'style-loader', 'css-loader' ]
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                babelrc: false,
+                plugins: [
+                  require.resolve('babel-plugin-transform-es2015-modules-commonjs'),
+                  require.resolve('styled-jsx/babel'),
+                ]
+              }
+            },
+            'styled-jsx-css-loader'
+          ]
         }
       ]
     },
@@ -26,6 +49,30 @@ module.exports = {
       exclude: /node_modules/,
       query: {
         presets: ['es2015']
+      }
+    },
+    webpack: (config) => {
+      config.module.rules.push(
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'emit-file-loader',
+              options: {
+                name: 'dist/[path][name].[ext].js',
+              },
+            },
+            {
+              loader: 'babel-loader',
+              options: {
+                babelrc: false,
+                extends: path.resolve(__dirname, './.babelrc'),
+              },
+            },
+            'styled-jsx-css-loader',
+          ],
+        }
+      );
+      return config;
     }
-}
 };
