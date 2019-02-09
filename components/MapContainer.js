90d95ccc8f6ac97d38343f852e4dcec97a79667a
jlemus1234 <jlemus1234@gmail.com>
2019-02-09 14:34:05 -0500

    Reverting to defaults

--- components/MapContainer.js
+++ components/MapContainer.js
@@ -27,6 +27,7 @@ import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 import GoogleMap from "google-map-react";
 
+var Popup;
 class MapContainer extends React.Component {
 
 	constructor(props) {
@@ -53,16 +54,25 @@ class MapContainer extends React.Component {
         zoom: 9,
     };
 
+
+    
+
     // this may only occur once the the api loads, which only occurs once, despite any changes to the props,etc
     // maps is the API object. Allows you to use functions like geocoding
     // map is our actual map 
+
     renderMarkers(map, maps) {
         let MapContainer = this;
         this.state.maps = maps;
         this.state.map = map;
         const Geocoder = new maps.Geocoder();   //converts address to lat/lng
+/*
+        Popup = createPopupClass();
+        var popup = new Popup(     
+            new google.maps.LatLng(-33.866, 151.196),
+            document.getElementById('content'));
+*/        
 
-        var Popup = createPopupClass();
 
         function createInfoWindow(map, maps, marker, title) {
             console.log("Creating info window")
@@ -81,76 +91,7 @@ class MapContainer extends React.Component {
             })
         }
 
-/////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////
 
-        function createPopupClass() {
-            /**
-             * A customized popup on the map.
-             * @param {!google.maps.LatLng} position
-             * @param {!Element} content The bubble div.
-             * @constructor
-             * @extends {google.maps.OverlayView}
-             */
-            function Popup(position, content) {
-              this.position = position;
-          
-              content.classList.add('popup-bubble');
-          
-              // This zero-height div is positioned at the bottom of the bubble.
-              var bubbleAnchor = document.createElement('div');
-              bubbleAnchor.classList.add('popup-bubble-anchor');
-              bubbleAnchor.appendChild(content);
-          
-              // This zero-height div is positioned at the bottom of the tip.
-              this.containerDiv = document.createElement('div');
-              this.containerDiv.classList.add('popup-container');
-              this.containerDiv.appendChild(bubbleAnchor);
-          
-              // Optionally stop clicks, etc., from bubbling up to the map.
-              google.maps.OverlayView.preventMapHitsAndGesturesFrom(this.containerDiv);
-            }
-            // ES5 magic to extend google.maps.OverlayView.
-            Popup.prototype = Object.create(google.maps.OverlayView.prototype);
-          
-            /** Called when the popup is added to the map. */
-            Popup.prototype.onAdd = function() {
-              this.getPanes().floatPane.appendChild(this.containerDiv);
-            };
-          
-            /** Called when the popup is removed from the map. */
-            Popup.prototype.onRemove = function() {
-              if (this.containerDiv.parentElement) {
-                this.containerDiv.parentElement.removeChild(this.containerDiv);
-              }
-            };
-          
-            /** Called each frame when the popup needs to draw itself. */
-            Popup.prototype.draw = function() {
-              var divPosition = this.getProjection().fromLatLngToDivPixel(this.position);
-          
-              // Hide the popup when it is far out of view.
-              var display =
-                  Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000 ?
-                  'block' :
-                  'none';
-          
-              if (display === 'block') {
-                this.containerDiv.style.left = divPosition.x + 'px';
-                this.containerDiv.style.top = divPosition.y + 'px';
-              }
-              if (this.containerDiv.style.display !== display) {
-                this.containerDiv.style.display = display;
-              }
-            };
-          
-            return Popup;
-          }
-
-/////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////
 
 		//render marker at bisexual resource center (also the default center)
         Geocoder.geocode({"address": "Bisexual Resource Center"}, function(results, status) {
@@ -275,5 +216,7 @@ class MapContainer extends React.Component {
 		);
 	}
 }
+
+
 
 export default MapContainer;
