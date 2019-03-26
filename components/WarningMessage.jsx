dbfd0ecf1e27b590c1666829e5548550e0107c1d
Steph Xu <stephxuzy96@gmail.com>
2019-03-26 00:17:08 -0400

    Revert "warning message and infobar msg"
    This reverts commit b7b262d717b594009961632309a6b403f77fddb9.

--- components/WarningMessage.jsx
+++ components/WarningMessage.jsx
@@ -1,8 +1,8 @@
-import React, { PureComponent } from 'react';
+import { Component } from 'react';
 
 const warningTitle = {
   color: '#F293C1',
-  fontSize: '40px',
+  fontSize: '50px',
   fontWeight: 'bold',
 };
 
@@ -17,36 +17,40 @@ const innerContainter = {
 };
 
 const warningMessageContent = {
-  color: '#707070',
-  fontSize: '15px',
+  color: 'black',
+  fontSize: '18px',
   marginTop: '10px',
   borderTop: '5px solid #F293C1',
   paddingTop: '10px',
 };
 
 const contactLink = {
-  fontSize: '16px',
+  fontSize: '21px',
   color: '#F293C1',
   fontWeight: 'bold',
   textDecoration: 'none',
 };
 
-const WarningMessage = () => (
-  <div style={innerContainter}>
-    <div style={warningTitle}>Oh No!</div>
-    <div style={warningMessageContent}>
-                No resource centers seem to be found around you in our database. Make sure its address is valid.
-                If you belong to a group that you would like to have listed here, please
-      {' '}
-      <a href="mailto: brc@biresource.org" style={contactLink}>drop us an email</a>
-      {' '}
-      at brc@biresource.org with information about your group and the URL or email contact to use.
-      <br />
-      {' '}
-      <br />
-                Here is the list of all the resource centers we currently have:
-    </div>
-  </div>
-);
+class WarningMessage extends Component {
+  render() {
+    return (
+      <div style={innerContainter}>
+        <div style={warningTitle}>Oh No!</div>
+        <div style={warningMessageContent}>
+                    No resource centers seem to be found around you in our database. Make sure its address is valid.
+                    If its address is valid, then it's possible that our database has not updated this resource center yet,
+                    please
+          {' '}
+          <a href="mailto: brc@biresource.org" style={contactLink}>contact us.</a>
+          {' '}
+          <br />
+          {' '}
+          <br />
+                    Here is the list of all the resource centers we currently have:
+        </div>
+      </div>
+    );
+  }
+}
 
 export default WarningMessage;
