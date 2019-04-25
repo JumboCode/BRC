54b46bf0a8a452e8f01c18ba0bcf9d375699c8e7
Steph Xu <stephxuzy96@gmail.com>
2019-04-25 10:00:23 -0400

    improved email validation

--- pages/suggestion.jsx
+++ pages/suggestion.jsx
@@ -145,7 +145,7 @@ class Suggestion extends Component {
 
   validateEmail = (email) => {
     const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+    return re.test(email.trim());
   }
 
   validateEntry = (name, val) => {
